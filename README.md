eee3e7c3c0b81156bd69446f917c74481cc5fd62
Ben-85 <ben.neuman85@gmail.com>
2022-03-02 20:49:51 -0500

    Added UI element

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Rock Paper Scissors project for The Odin Project.
 
 Initial Commit: added blank index.html with script running console version of Rock, Paper, Scissors.
+
+Next major commit: added ui interface.
--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function playerPlay() {
-    let choice = prompt("Choose rock, paper or scissors.").toLowerCase();
-    
-    while (choice.match(/^(rock|paper|scissors)$/) == null) {
-        alert(`${choice} is an invalid choice. Please choose rock, paper or scissors`);
-        choice = prompt("Choose rock, paper or scissors.").toLowerCase();
-    }
-    
-    return choice;
-}
-
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 4);
     if (randomNumber == 1) {
@@ -24,52 +13,52 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        return [`It's a tie! You both chose ${computerSelection}!`, "tie"];
+        roundResult.textContent = `Round ${round}: You both picked ${playerSelection}, it's a tie!`;
+        return 'tie';
     }
     else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
             (playerSelection == 'paper' && computerSelection == 'rock') ||
             (playerSelection == 'scissors' && computerSelection == 'paper')) {
-                return [`Winner! ${playerSelection[0].toUpperCase()}${playerSelection.slice(1)} beats ${computerSelection}!`, "player"];
+                roundResult.textContent = `Round ${round}: ${playerSelection[0].toUpperCase()}${playerSelection.slice(1)} beats ${computerSelection}, you win!`;
+                return 'player';
             }
     else {
-        return [`Loser! ${computerSelection[0].toUpperCase()}${computerSelection.slice(1)} beats ${playerSelection}!`, "computer"];
-    }
-}
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    for (let i = 0; i < 5; i++) {
-        let results = playRound(playerPlay(), computerPlay());
-        alert(`Round ${i + 1}: ` + results[0]);
-        console.log(`Round ${i + 1}: ` + results[0]);
-        
-        if (results[1] == 'player') {
-            playerScore++;
-        }
-        
-        else if (results[1] == 'computer') {
-            computerScore++;
-        }
-    }
-    
-    console.log(`------Final Score------\n     Player: ${playerScore}\n     Computer: ${computerScore}\n-----------------------`);
-    alert(`------Final Score------\n     Player: ${playerScore}\n     Computer: ${computerScore}\n-----------------------`);
-    
-    if (playerScore > computerScore) {
-        console.log('Player wins!');
-        alert('Player wins!')
-    }
-    
-    else if (playerScore < computerScore) {
-        console.log('Computer wins!');
-        alert('Computer wins!');
-    }
-
-    else {
-        console.log("It's a tie!");
-        alert("It's a tie!");
+        roundResult.textContent = `Round ${round}: ${computerSelection[0].toUpperCase()}${computerSelection.slice(1)} beats ${playerSelection}, you lose!`;
+        return 'computer';
     }
 }
 
-game();
+const buttons = document.querySelectorAll('button');
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
+
+roundResult.textContent = "Click your choice to play Rock Paper Scissors!";
+scoreBoard.textContent = "First one to 5 wins!"
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      let result = playRound(button.id, computerPlay());
+      if (result == 'player') {
+          playerScore++;
+      } else if (result == 'computer') {
+          computerScore++;
+      } 
+      
+      ++round;
+      
+      if (playerScore < 5 && computerScore < 5) {
+          scoreBoard.textContent = `Score: Player: ${playerScore} Computer: ${computerScore}`;
+      } else {
+          if (playerScore > computerScore) {
+              scoreBoard.textContent = `You won ${playerScore} to ${computerScore}!\nMake another selection to play again!`;
+          }
+          else {
+              scoreBoard.textContent = `You lost ${computerScore} to ${playerScore}!\nMake another selection to play again!`;
+          }
+          round = 1;
+          playerScore = 0;
+          computerScore = 0;
+      }
+    });
+});
